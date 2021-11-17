2e69d0d7eb8f95c5716f6e13bfecbda9f415e84b
Aymeric Erulin <erulin.aymeric@gmail.com>
2021-11-17 17:37:00 +0100

    Seed and Display flats

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 100 fake flats..'
+100.times do
+  flat = Flat.new(
+    name: Faker::Address.community,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    number_of_guests: rand(1..6),
+    description: Faker::Lorem.paragraphs,
+    price_per_night: rand(50..400),
+    image: "https://unsplash.it/400/400?image=#{rand(1000)}"
+  )
+  flat.save!
+end
+puts 'Finished!'
